1e8d32404b42cb6a4e86ca9a17b2b3ffd1ab10da
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-09-30 14:53:20 +0300

    add primary colour and splash

---
+++ CSV Parser/CSV Parser/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extensions.swift
+//  CSV Parser
+//
+//  Created by Stavros Tsikinas on 30/9/22.
+//
+
+import UIKit
+
+
+extension UIColor {
+    
+    static var primaryColor: UIColor {
+        UIColor(red: 0.08, green: 0.322, blue: 0.314, alpha: 1.0)
+    }
+    
+}
