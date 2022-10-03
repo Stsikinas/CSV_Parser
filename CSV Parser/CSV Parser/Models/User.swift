c027e91281b82d8a88f1069b6f4483edde78fb19
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-10-03 11:58:15 +0300

    create user model

---
+++ CSV Parser/CSV Parser/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  CSV Parser
+//
+//  Created by Stavros Tsikinas on 3/10/22.
+//
+
+import Foundation
+
+
+struct User {
+    
+    let firstName: String
+    let lastName: String
+    let stars: Int
+    let dob: String
+    
+}
